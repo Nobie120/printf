c32391e31981e4291711ed6cfb15771285c7f227
Nobie120 <n12507400@gmail.com>
2023-08-20 15:17:08 -0700

    Function to print characters

--- handle_characters.c
+++ handle_characters.c
@@ -3,13 +3,12 @@
 /**
  * handle_characters - print characters
  * @args:The arguement list
- * Number of characters printed
+ * Return:Number of characters printed
  */
 
 int handle_characters(va_list args)
 {
 	char c = va_arg(args, int);
-
 	write(1, &c, 1);
 
 	return (1);
