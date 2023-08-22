f5f270cbcadfcf40e3310828d6eb1cf672519619
Andrea29lwehumbiza <andrealwehumbiza@gmail.com>
2023-08-22 06:11:09 -0700

    2

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	_printf("I%c %s %%  %d\n", 'm', "ken", 990);
+	_printf("%c\n", 'm');
+	_printf("jhfjf\n");
+	_printf("My %ame is %s Im %d years old\n", 'n', "ken", 19);
+	_printf("%b\n", 98);
+	return 0;
+}
